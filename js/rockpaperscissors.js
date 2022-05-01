59c5a7fc4d5992eea2a5c20fdefd000478dd833d
SimonO7 <sdragonz461@gmail.com>
2022-05-01 15:54:29 -0700

    Create function for generating computer play

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,1 +1,10 @@
-console.log("Under construction");
+function computerPlay() {
+    //Create a list with elements "Rock", "Paper", and "Scissors"
+    const options = ["Rock", "Paper", "Scissors"];
+    //Generate a random number between 0 and 2. That will be the index of the element.
+    const index = Math.floor(Math.random()*3);
+    //Return the element from that index.
+    return options[index];
+}
+
+console.log(computerPlay());
