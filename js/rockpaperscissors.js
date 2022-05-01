b5a97e72fada2bb4d0ad981f32b4674ffe802a5e
SimonO7 <sdragonz461@gmail.com>
2022-05-01 16:05:35 -0700

    Add documentation to computerPlay function

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,4 +1,10 @@
 function computerPlay() {
+    /**
+     * Randomly choose a hand, either rock, paper, or scissors, and return it.
+     * @param none
+     * @return {string} one of "Rock", "Paper", or "Scissors"
+     */
+
     //Create a list with elements "Rock", "Paper", and "Scissors"
     const options = ["Rock", "Paper", "Scissors"];
     //Generate a random number between 0 and 2. That will be the index of the element.
